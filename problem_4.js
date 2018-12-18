03c7e2874146afa1f1be3a383ba7f5347de546b8
bguillou <brieuc.guillou-ext@elis.com>
2018-12-18 14:05:27 +0100

    problem 4

---
+++ problem_4.js
@@ -0,0 +1,54 @@
+function isPalidromique (num) {
+    var s = num.toString();
+    for (var index = 0; index < s.length/2 ; index++) {
+        var a = s[index];
+        var b = s[s.length - index - 1];
+
+        if (a !== b) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var result = isPalidromique(99);
+console.log(result);
+
+function exo4(max) {
+    var palidroms = [];
+    for (var a = max; a>0; a--){
+        for (var b = max; b > 0; b--) {
+            x = a * b;
+            if (isPalidromique(x)) {
+                palidroms.push(x);
+            }
+        }
+    }
+    var max = palidroms.reduce((e, max) => e > max ? e : max, 0)
+    console.log(palidroms.length);
+    console.log(max);
+}
+
+// result = exo4(999);
+
+function exo4Bis(max) {
+    var palidroms = [];
+    for (var a = max; a > max/10; a--) {
+        for (var b = max; b > max/10; b--) {
+            x = a * b;
+            if (isPalidromique(x)) {
+                var result = {
+                    'x': x,
+                    'a': a,
+                    'b': b,
+                }
+                palidroms.push(result);
+            }
+        }
+    }
+    var max = palidroms.reduce((e, max) => e.x > max.x ? e : max, { 'x': 0 })
+    console.log(palidroms.length);
+    console.log(max);
+}
+
+result = exo4Bis(999);
